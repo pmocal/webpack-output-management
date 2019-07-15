ee04c1cadd5f92e24ee38a64c087ff912ab0bf0a
Parthiv Mohan <parthiv.mohan@gmail.com>
2019-07-15 15:45:37 -0400

    edits for heroku deploy

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname + '/dist'));
+
+// send the user to index html page inspite of the url
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
+});
+
+app.listen((process.env.PORT || 8080), function () {
+  console.log('webpack output management app listening on port!');
+});
